7ad0c2a916628eb45a69a991758800f30d6c115a
Toma Nistor <toma.nistor@gmail.com>
2018-04-22 02:56:49 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>View Meetups</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar fixed dark class="primary">
+      <v-toolbar-title>Stoaty</v-toolbar-title>
+      <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left>supervisor_account</v-icon> View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    },
+    name: 'App'
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.lightBlue.darken4, // #01579B
+    secondary: colors.lightBlue.lighten4, // #B3E5FC
+    accent: colors.cyan.lighten2 // #4DD0E1
+  }
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
